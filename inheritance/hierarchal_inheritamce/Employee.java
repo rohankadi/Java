2e0ded935c6eee4fb137ba1fac8b9701dc7cba03
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-24 20:26:05 +0530

    Inheritance

---
+++ inheritance/hierarchal_inheritamce/Employee.java
@@ -0,0 +1,12 @@
+package com.kn.hierarchal_inheritamce;
+
+public class Employee {
+	String name;
+	int salary;
+	
+	//parameterized constructor
+	public Employee(String name, int salary) {
+		this.name = name;
+		this.salary = salary;
+	}
+}
