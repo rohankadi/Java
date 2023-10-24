04c8c62054b75f77d22ace07cb239f9e74997c52
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-24 20:37:44 +0530

    Constructor

---
+++ constructor/ParentAppDemo.java
@@ -0,0 +1,30 @@
+package com.kn.constructor;
+
+public class ParentAppDemo {
+
+	public static void main(String[] args) {
+		//Child c1 = new Child();
+//		c1.name = "Rohan";
+//		c1.age = 22;
+		
+//		System.out.println("Name : "+ c1.name + ", Age : "+ c1.age );
+//		
+//		System.out.println("************************");
+		
+//		Child c2 = new Child("Ram", 21);
+//		System.out.println("Name : "+ c2.name + ", Age : "+ c2.age );
+		
+//		System.out.println("************************");
+//		
+//		Child c3 = new Child("Rohan");
+//		System.out.println("Name : "+ c3.name + ", Age : "+ c3.age );
+//		
+//		System.out.println("************************");
+//		
+		Child c4 = new Child(8);
+		System.out.println("Name : "+ c4.name + ", Age : "+ c4.age );
+	
+	}
+	
+	
+}
