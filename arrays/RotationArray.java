59a1f4ce4f568c1d5e4d3a6033b177c78e295cd5
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-25 08:51:10 +0530

    ArrayMatrix

---
+++ arrays/RotationArray.java
@@ -0,0 +1,40 @@
+package com.kn.arraymatrix;
+
+import java.util.Scanner;
+
+public class RotationArray {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int[] arr = { 10, 20, 30, 40, 50 };
+		int[] brr = { 11, 12, 13, 14, 15 };
+		int[] crr = new int[arr.length + brr.length];
+		
+		int[] drr = merging(arr, brr, crr);
+		for(int i=0; i<drr.length; i++) {
+			System.out.print(drr[i] + " ");
+		}
+
+		scan.close();
+	}
+
+	private static int[] merging(int[] arr, int[] brr, int[] crr) {
+		int a = 0;
+		int b = brr.length-1;
+		for (int i = 0; i < crr.length; i++) {
+			if(i % 2 == 0) {
+				crr[i] = arr[a];
+				a++;
+			}
+			else {
+				crr[i] = brr[b];
+				b--;
+			}
+		}
+		return crr;
+	}
+
+
+	}
+
+
