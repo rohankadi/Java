240c2def9c87b058bfdb4e6ef25e4b68dfd54ca6
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-26 22:57:40 +0530

    TypeCasting

---
+++ TypeCasting/Variable_Typecasting_A.java
@@ -0,0 +1,13 @@
+package com.object.typeCasting;
+
+class Variable_Typecasting_A {
+	int x = 777;
+}
+
+class Variable_Typecasting_B extends Variable_Typecasting_A{
+	int x = 888;
+}
+
+class Variable_Typecasting_C extends Variable_Typecasting_B{
+	int x = 999;
+}
