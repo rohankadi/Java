409bba0eac066a71874d3ffa7ed22e2002bb91d3
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-26 22:54:34 +0530

    packages

---
+++ packages/Packages.java
@@ -0,0 +1,44 @@
+package com.packages;
+
+public class Packages {
+
+	public static void main(String[] args) {
+		/* Java source-file structure
+		 PACKAGES : (are Encapsulation Mechanism)
+		  It's a group of related CLASSES and INTERFACES present inside a single unit.     
+	    */
+		
+		/*Advantages of using packages
+		 * 1. Comflix /Problem naming we can resolved
+		 * 2. Modularity
+		 * 3. Maintainability
+		 * 4. Security
+		 */
+		
+		/*
+		Universal Naming convention is used for package
+		ex : if icici bank url is => icici.com
+		then, we can give 
+		com.icici; as a package name
+		
+	   ====> use Internet domain name reverse as package naming convention for unique identification
+		*/
+		
+		
+		/************************* IMPORTANT POINTS ***************************
+		1) In any Java source file at most 1 package should be allowed not more than that
+		2) 1st non comment statement should be package
+		
+		ex : 
+		  package com.durgasoft.ocja;
+		  import java.util.*;
+		 
+		 Flow =====>
+		 1st => package statement (at most one package should be allowed)
+		 2nd => import statement  (any number of import statements are allowed)
+		 3rd => class or interface or enum  (any number of statements are allowed)
+		 */
+	}	
+}
+
+
