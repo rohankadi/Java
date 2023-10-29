af7f683267e242c4fd06be1b39adb292a661657c
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-29 12:59:03 +0530

    CompositionAndAggregation

---
+++ CompositionAndAggregation/CompAggEx2/Heart.java
@@ -0,0 +1,12 @@
+package com.rk.CompAggEx2;
+
+public class Heart {
+	 String color;
+	 int weight;
+	 
+	public Heart(String color, int weight) {
+		this.color = color;
+		this.weight = weight;
+	}
+	 
+}
