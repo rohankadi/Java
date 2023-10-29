84673ff73f2200faece4af97e9b4c404d2714533
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-29 12:54:49 +0530

    CompositionAndAggregation

---
+++ CompositionAndAggregation/CompAggEx2/Book.java
@@ -0,0 +1,14 @@
+package com.rk.CompAggEx2;
+
+public class Book {
+
+	String author;
+	int price;
+	String bookname;
+	
+ 	public Book(String author, int price, String bookname) {
+		this.author = author;
+		this.price = price;
+		this.bookname = bookname;
+	}
+}
