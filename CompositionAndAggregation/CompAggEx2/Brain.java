b9464ac54f058b8cc4bd4b4c240b44147fa9c50e
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-29 12:55:19 +0530

    CompositionAndAggregation

---
+++ CompositionAndAggregation/CompAggEx2/Brain.java
@@ -0,0 +1,12 @@
+package com.rk.CompAggEx2;
+
+public class Brain {
+    String color;
+    int weight;
+    
+    //constructor
+	public Brain(String color, int weight) {
+		this.color = color;
+		this.weight = weight;
+	} 
+}
