b4881e5912c5d7b703ec2fec84eb7842b7eb6f55
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-30 22:20:36 +0530

    CompositionAndAggregation

---
+++ CompositionAndAggregation/CompositionAndAggregation/OS.java
@@ -0,0 +1,13 @@
+package com.rk.CompositionAndAggregation;
+
+public class OS {
+
+	String name;
+	int type;
+	
+	public OS(String name, int type) {
+		super();
+		this.name = name;
+		this.type = type;
+	}
+}
