d31558cade771ad4eeb23467a1cc2b513a494cd1
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-30 21:52:57 +0530

    CompAgg

---
+++ CompositionAndAggregation/CompAggExample/Book.java
@@ -0,0 +1,13 @@
+package com.rk.CompAggExample;
+
+public class Book {
+	String author;
+	int price;
+	
+	
+	public Book(String author , int price) {
+		super();
+		this.author = author;
+		this.price = price;
+	}
+}
