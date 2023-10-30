92cc01bad6f1e4a020bc2abf656c5995715d5702
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-30 22:19:15 +0530

    CompositionAndAggregation

---
+++ CompositionAndAggregation/CompositionAndAggregation/Charger.java
@@ -0,0 +1,12 @@
+package com.rk.CompositionAndAggregation;
+
+public class Charger {
+
+	String brand;
+	String color;
+	
+	public Charger(String brand, String color) {
+		this.brand = brand;
+		this.color = color;
+	}
+}
