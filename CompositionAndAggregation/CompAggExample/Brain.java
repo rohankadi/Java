3de48550f365bc2dcc9afcdebc2738ce256b2cb6
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-30 21:54:39 +0530

    CompAgg

---
+++ CompositionAndAggregation/CompAggExample/Brain.java
@@ -0,0 +1,13 @@
+package com.rk.CompAggExample;
+
+public class Brain {
+
+	String color;
+	int weight;
+	
+	public Brain(String color, int weight) {
+		super();
+		this.color = color;
+		this.weight = weight;
+	}
+}
