0604a71de11070d7050da00256ade33d935d86d4
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-31 21:38:30 +0530

    OOPS

---
+++ ShapeOopProject/Rectangle.java
@@ -0,0 +1,30 @@
+package com.rk.ShapeOopProject;
+
+import java.util.Scanner;
+
+public final class Rectangle extends Shape {
+	private Scanner scan = new Scanner(System.in);
+	private float length = 0.0f;
+	private float breadth = 0.0f;
+	private float area = 0.0f;
+	
+	@Override
+	public void takeinput() {
+		System.out.println("Enter length = ");
+		length = scan.nextFloat();
+		System.out.println("Enter width = ");
+		breadth = scan.nextFloat();
+	}
+
+	@Override
+	public void calculateArea() {
+		area = length * breadth;
+	}
+
+	@Override
+	public void displayArea() {
+		System.out.println("Area = " + area);
+
+	}
+
+}
