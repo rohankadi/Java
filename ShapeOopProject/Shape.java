eeee1905b2a701f42b31e7cab83869cb7171bbcf
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-31 21:38:48 +0530

    OOPS

---
+++ ShapeOopProject/Shape.java
@@ -0,0 +1,9 @@
+package com.rk.ShapeOopProject;
+
+public abstract class Shape {
+	
+	abstract public void takeinput();
+	abstract public void calculateArea();
+	abstract public void displayArea();
+	
+}
