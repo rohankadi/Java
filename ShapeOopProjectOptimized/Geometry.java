17be666bf09d9d1419e221cfcb6ea46bc82a4f62
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-10-31 21:52:01 +0530

    OOPS

---
+++ ShapeOopProjectOptimized/Geometry.java
@@ -0,0 +1,11 @@
+package com.rk.ShapeOopProjectOptimized;
+
+public class Geometry {
+
+	public void access(Shape ref) {
+		ref.takingInput();
+		ref.calculateArea();
+		ref.displayArea();
+	}
+	
+}
