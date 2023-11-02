290fbaa10c988853b64264800565a7503ba8b868
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-02 22:19:43 +0530

    Interface

---
+++ Interface/InterfaceChecking/Interface3.java
@@ -0,0 +1,9 @@
+package com.InterfaceChecking;
+/*
+//ERROR : Syntax error on token "implements", permits expected
+public interface Interface3 implements Interface2{
+
+	//implements => giving body
+}
+
+ */
