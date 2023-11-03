f914e4963f3ef2dbf7f8fd67fee0f9c76d7db774
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-03 13:08:37 +0530

    Interface

---
+++ Interface/rk/interface_MultipleInheritance/CalculatorDemo.java
@@ -0,0 +1,10 @@
+package com.rk.interface_MultipleInheritance;
+
+public class CalculatorDemo {
+
+	public static void main(String[] args) {
+
+		MyCalculator calci = new MyCalculator();
+		calci.add();
+	}
+}
