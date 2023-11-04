56f4b72ebffd7d222a0e88fed86b2c16057f11d6
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-04 19:59:23 +0530

    abstraction

---
+++ Abstraction/Teacher.java
@@ -0,0 +1,12 @@
+package com.kn.Abstraction;
+
+public abstract class Teacher {
+
+	// only have declaration not implementation, implementation is given by child
+	abstract void markAttendance();
+
+	abstract void teach();
+
+	abstract void doExp();
+
+}
