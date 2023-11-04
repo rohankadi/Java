dc493563f9f90eda4d910228ed6dd41fb14b15e0
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-04 19:57:10 +0530

    HybridInheritance

---
+++ inheritance/hybrid_inheritance/KodOmTech.java
@@ -0,0 +1,41 @@
+package com.hybrid.inheritance;
+
+public class KodOmTech implements EdTech {
+
+	@Override
+	public void buildSoftware() {
+		System.out.println("KodNest Builds a software");
+	}
+
+	@Override
+	public void teach() {
+		System.out.println("Institute have Best Teaching staff");
+	}
+
+	@Override
+	public void opportunities() {
+		System.out.println("There are many Opportunities present");
+	}
+
+	@Override
+	public void developProduct() {
+		System.out.println("OmNest develops Product");
+	}
+
+	@Override
+	public void buildProject() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void qualityAssurance() {
+		System.out.println("This institute have quality assurance");
+	}
+
+	@Override
+	public void qualityControl() {
+		System.out.println("This institute have quality control room");
+	}
+
+}
