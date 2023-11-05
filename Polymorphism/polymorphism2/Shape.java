63fde327cf944de7ae8e4d80171c5206438833dd
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-05 10:19:00 +0530

    polymorphism

---
+++ Polymorphism/polymorphism2/Shape.java
@@ -0,0 +1,11 @@
+package com.kn.polymorphism2;
+
+public class Shape {
+	public void draw() {
+		System.out.println("drawing Shape");
+	}
+	
+	public double calculateArea() {
+		return 5.5 * 10.5;
+	}
+}
