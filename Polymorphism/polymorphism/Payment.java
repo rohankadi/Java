420d9bc8b89459eb7ec05c25708a84b11616a394
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-05 10:09:09 +0530

    polymorphism

---
+++ Polymorphism/polymorphism/Payment.java
@@ -0,0 +1,16 @@
+package com.kn.polymorphism;
+
+public class Payment {
+
+	public void bill() {
+		System.out.println("Bill generated");
+	}
+	
+	public void offer() {
+		System.out.println("PromoCode applied");
+	}
+	
+	public void pay() {
+		System.out.println("Payment done");
+	}
+}
