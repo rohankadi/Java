f72fe09c4523a88a490776a20f010fbf089c2eee
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-06 09:25:02 +0530

    polymorphism

---
+++ Polymorphism/polymorphism3/SoftwareEngineer.java
@@ -0,0 +1,12 @@
+package com.kn.polymorphism3;
+
+public class SoftwareEngineer {
+	
+	public void attendMeeting() {
+		System.out.println("Engineer attending meeting");
+	}
+	
+	public void writeTestCases() {
+		System.out.println("Engineer writing test Cases");
+	}
+}
