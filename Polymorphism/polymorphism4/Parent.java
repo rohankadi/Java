4dbe88d1c5b44592225a2e4ae3c481d63eecd6e5
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-06 09:33:57 +0530

    polymorphism

---
+++ Polymorphism/polymorphism4/Parent.java
@@ -0,0 +1,58 @@
+package com.kn.polymorphism4;
+
+ class Parent {
+	//01 Parent and Child have same access modifier
+	/*
+	public void display() {
+		System.out.println("This is Parent");
+	}
+	*/
+	//02 Parent and Child have same access modifier
+	/*
+	 void display() {
+	 System.out.println("This is Parent");	
+	}
+	*/
+	 
+	 //03
+	 /*
+	 public void display() {
+			System.out.println("This is Parent");
+		}
+		*/
+	 
+	 /*
+	 //04
+	  void display() {
+			System.out.println("This is Parent");
+		}
+	 */
+	 /*
+	  //05
+	protected void display() {
+			System.out.println("This is Parent");
+		}
+		*/
+	 
+	 //06
+	public void display() {
+		System.out.println("This is Parent");
+		}
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+			
+}
