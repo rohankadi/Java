1d54ac8d3e7889a2f87849793c4a24fc383a4788
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-09 23:22:30 +0530

    Overloading

---
+++ Method_Overloading/Test2.java
@@ -0,0 +1,13 @@
+package com.method.overloading;
+
+public class Test2 {
+
+	//overloaded methods
+	public void m1(Animal a) {
+		System.out.println("Animal Version");
+	}
+
+	public void m1(Monkey m) {
+		System.out.println("Monkey Version");
+	}
+}
