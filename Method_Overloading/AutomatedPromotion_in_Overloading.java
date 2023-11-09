faaa14bc510fa0e4d402adb1e85c1152fedebb17
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-09 23:19:15 +0530

    Overloading

---
+++ Method_Overloading/AutomatedPromotion_in_Overloading.java
@@ -0,0 +1,13 @@
+package com.method.overloading;
+
+public class AutomatedPromotion_in_Overloading {
+
+	public void m1(int i) {
+		System.out.println("int-arg type");
+	}
+	
+	public void m1(float f) {
+		System.out.println("float-arg type");
+	}
+	
+}
