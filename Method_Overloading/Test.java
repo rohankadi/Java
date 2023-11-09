eda1d2846ce9bcbe62f7eefa7e484b503dcb5d9b
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-09 23:22:00 +0530

    Overloading

---
+++ Method_Overloading/Test.java
@@ -0,0 +1,11 @@
+package com.method.overloading;
+
+public class Test {
+	public void m1(Object o) {
+		System.out.println("Object Version");
+	}
+	
+	public void m1(String s) {
+		System.out.println("String Version");
+	}
+}
