2017bd35d18a509e38acd074ca6a248206b4ff2a
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-10 21:55:04 +0530

    Delete anonymousInnerObjectCreation directory

--- anonymousInnerObjectCreation/Institute.java
+++
@@ -1,5 +0,0 @@
-package com.kn.anonymousInnerObjectCreation;
-
-public abstract class Institute {
-	abstract void teach();
-}
--- anonymousInnerObjectCreation/Shape.java
+++
@@ -1,5 +0,0 @@
-package com.kn.anonymousInnerObjectCreation;
-
-public interface Shape {
-	void display();
-}
