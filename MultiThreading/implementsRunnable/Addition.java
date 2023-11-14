a77238a76b8578e1e142c36ca39e6a52308fced8
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-14 20:32:38 +0530

    Add files via upload

---
+++ MultiThreading/implementsRunnable/Addition.java
@@ -0,0 +1,21 @@
+package com.rk.multithreading.implementsRunnable;
+
+public class Addition implements Runnable {
+
+	@Override
+	public void run() {
+		try {
+			System.out.println("Addition activity started");
+			int a = 1234;
+			int b = 9876;
+			// temporarily stops execution
+			Thread.sleep(5000);
+
+			int c = a + b;
+			System.out.println("Addition result is " + c);
+			System.out.println("Addition activity ended");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
