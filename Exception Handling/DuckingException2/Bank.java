1e31302af33bb4c94f3a04ab89bc47eeb8529e69
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-15 22:48:19 +0530

    DuckingException

---
+++ Exception Handling/DuckingException2/Bank.java
@@ -0,0 +1,20 @@
+package com.rk.DuckingException2;
+
+public class Bank {
+
+	// leaving without handling =>(throwing an exception to caller)
+	public void initiate() {
+		System.out.println("Connection 2 Established");
+
+		ATM atm = new ATM();
+		try {
+			atm.withdraw();
+		} 
+		catch (Exception e) {
+			System.out.println("EXCEPTION HANDLED BY BANK");
+			// e.printStackTrace();
+		}
+
+		System.out.println("Connection 2 Terminated");
+	}
+}
