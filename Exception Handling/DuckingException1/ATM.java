9d65631ce123844fc30d6c29e8750129a62b4635
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-15 22:46:19 +0530

    DuckingException

---
+++ Exception Handling/DuckingException1/ATM.java
@@ -0,0 +1,23 @@
+//03rd Approach => Ducking the Exception
+package com.rk.DuckingException1;
+
+import java.util.Scanner;
+
+public class ATM {
+
+	//leaving without handling =>(throwing an exception to caller)
+	public void withdraw() throws Exception{
+
+		System.out.println("Connection 1 Established");
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter accno and pwd");
+		int acc = scan.nextInt();
+		int pwd = scan.nextInt();
+
+		int res = acc / pwd;
+
+		System.out.println("Take your money " + res);
+		scan.close();
+	}
+}
