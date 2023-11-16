e215e411ec88842af2a8fa929046d3b29a619c1d
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-16 21:55:56 +0530

    User Exception

---
+++ Exception Handling/CustomizedException1/KannadaEnglishMatrimoney.java
@@ -0,0 +1,11 @@
+package com.rk.CustomizedException1;
+
+public class KannadaEnglishMatrimoney {
+
+	public static void main(String[] args) {
+		
+		Matrimoney matrimoney = new Matrimoney();
+		matrimoney.acceptApplication();
+		
+	}
+}
