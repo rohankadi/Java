11853655571661ff8833a26e7e70385a58b02951
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-17 11:57:21 +0530

    Add files via upload

---
+++ Objects/Student_Tasks/Student.java
@@ -0,0 +1,15 @@
+package com.kn.Student_Tasks;
+
+public class Student {
+
+	int id;
+	String name;
+	int marks;
+	
+	
+	public Student(int id, String name, int marks) {
+		this.id = id;
+		this.name = name;
+		this.marks = marks;
+	}
+}
