c7e270249a45018a5b2ce906a2fb0400a686e6ec
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-18 22:23:45 +0530

    Add files via upload

---
+++ errors/App.java
@@ -0,0 +1,24 @@
+package com.rk.errors;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+	Demo d = new Demo();
+	d.alpha();
+	
+	}
+}
+
+/*
+Alphas Implementation
+Alphas Implementation
+Alphas Implementation
+Alphas Implementation
+Alphas Implementation
+Alphas Implementation
+ " "    "   "   " 
+Exception in thread "main" java.lang.StackOverflowError
+	at java.base/java.io.PrintStream.writeln(PrintStream.java:722)
+ */
+
