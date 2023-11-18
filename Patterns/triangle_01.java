5fc492f1074f7d8ab1c8e23c63d0dd2c3ed7337d
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-18 22:22:00 +0530

    Add files via upload

---
+++ Patterns/triangle_01.java
@@ -0,0 +1,37 @@
+//same as, Half_Pyramid_03_2 
+package com.kn.patterns;
+
+import java.util.Scanner;
+
+public class triangle_01 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter number of rows & columns to be created : ");
+		// Here, we have taken the value from the user.
+		int n = scan.nextInt();
+
+		printSquare(n);
+		scan.close();
+	}
+
+	public static void printSquare(int n) {
+		// This is the outer loop which will loop for the rows.
+		for (int i = 1; i <= n; i++) {
+			// This is the inner loop which loops for the columns
+			// no. of columns = row number for each line here.
+			for (int j = 1; j <= n; j++) {
+				if(i+j <= n) {
+				System.out.print(" ");
+			}
+				else {
+					System.out.print("* ");
+				}
+		}
+			// As soon as stars for each iteration are printed, we move to the
+			// next row and give a line break otherwise all stars
+			// would get printed in6 1 line.
+			System.out.println();
+		}
+	}
+}
