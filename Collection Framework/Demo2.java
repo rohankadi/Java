68ab18bf76f56803640476b0d32cf8474f09ca83
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-19 20:19:20 +0530

    Collection Framework

---
+++ Collection Framework/Demo2.java
@@ -0,0 +1,13 @@
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		
+		Employee e = new Employee("Rama");
+		System.out.println(e);
+		
+		String str = new String("Sita");
+		System.out.println(str);
+		
+	}	
+}
