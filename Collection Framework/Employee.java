38898a66a1f8612e8361233931c46f7c330af950
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-19 20:19:45 +0530

    Collection Framework

---
+++ Collection Framework/Employee.java
@@ -0,0 +1,17 @@
+
+public class Employee {
+
+	String name;
+
+	public Employee(String name) {
+		super();
+		this.name = name;
+
+	}
+
+	@Override
+	public String toString() {
+		return "Hello " + name;
+	}
+
+}
