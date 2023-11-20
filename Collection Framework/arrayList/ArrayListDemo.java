ea6e32235b97dd0cab414a7bf7561dfe821bec9e
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-20 22:48:55 +0530

    Add files via upload

---
+++ Collection Framework/arrayList/ArrayListDemo.java
@@ -0,0 +1,23 @@
+//ALL THE PROBLEMS ASSOCIATED WITH ARRAY IS SOLVED BY USING  ARRAYLIST
+
+package com.rk.arrayList;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+
+	public static void main(String[] args) {
+		
+		ArrayList al = new ArrayList();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		al.add("Raju");
+		al.add(3.147f);
+		al.add(true);
+		
+		System.out.println(al);
+		
+	}
+}
+
