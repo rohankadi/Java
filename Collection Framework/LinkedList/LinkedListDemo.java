108132f5f8385f799fe92f783fddee6316c559d8
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-20 22:57:31 +0530

    Add files via upload

---
+++ Collection Framework/LinkedList/LinkedListDemo.java
@@ -0,0 +1,20 @@
+package com.rk.LinkedList;
+import java.util.LinkedList;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		LinkedList ll = new LinkedList();
+		ll.add(10);
+		ll.add(20);
+		ll.add(30);
+		ll.add(true);
+		ll.add(150);
+		ll.add(300);
+		ll.add(3.147f);
+		ll.add("Raju");
+		
+		System.out.println(ll);		
+		
+	}
+}
