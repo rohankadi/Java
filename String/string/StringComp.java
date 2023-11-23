14e9614d92d8188600b05eda1b62e95d834a833b
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-23 23:05:36 +0530

    Add files via upload

---
+++ String/string/StringComp.java
@@ -0,0 +1,47 @@
+package com.kn.string;
+
+public class StringComp {
+
+	public static void main(String[] args) {
+		String s1 = "Ram";
+		String s2 = "Sita";
+		String s3 = "Ram" + "Sita";
+		String s4 = "Ram" + "Sita";
+
+		//01
+		System.out.println("Output for " + s1 +" and  " + s2 + ":");
+		// Reference Comparison
+		if (s1 == s2) {
+			System.out.println("Same reference");
+		} else {
+			System.out.println("Different reference");
+		}
+
+		// String Comparison
+		if (s1.equals(s2)) {
+			System.out.println("Same String");
+		} else {
+			System.out.println("Different String");
+		}
+
+		//02
+		System.out.println("******************************");
+		System.out.println("Output for " + s3 +" and  " + s4 + ":");
+		
+		// Reference Comparison
+		if (s3 == s4) {
+			System.out.println("Same reference");
+		} else {
+			System.out.println("Different reference");
+		}
+
+		// String Comparison
+		if (s3.equals(s4)) {
+			System.out.println("Same String");
+		} else {
+			System.out.println("Different String");
+		}
+
+	}
+
+}
