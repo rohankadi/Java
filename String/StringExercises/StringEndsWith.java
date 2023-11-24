a012add31c639b7aa24f6a7ea2eed74bd4a6df8d
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-24 18:52:16 +0530

    Add files via upload

---
+++ String/StringExercises/StringEndsWith.java
@@ -0,0 +1,12 @@
+//Check whether a given string ends with the contents of another string
+package com.kn.StringExercises;
+
+public class StringEndsWith {
+
+	public static void main(String[] args) {
+		String str = "Java Exercise";
+
+	   System.out.println(str.endsWith("se"));
+	   System.out.println(str.endsWith("es"));
+	}
+}
