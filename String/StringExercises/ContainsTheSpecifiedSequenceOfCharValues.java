faea34c97e9df904c839f463a28be4cc75737724
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-24 18:50:42 +0530

    Add files via upload

---
+++ String/StringExercises/ContainsTheSpecifiedSequenceOfCharValues.java
@@ -0,0 +1,11 @@
+//Write a Java program to test if a given string contains the specified sequence of char values.
+package com.kn.StringExercises;
+
+public class ContainsTheSpecifiedSequenceOfCharValues {
+	public static void main(String[] args) {
+		String s = "PHP Exercises and Python Exercises";
+		String search = "and";
+		
+		System.out.println(s.contains(search));
+	}
+}
