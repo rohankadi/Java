22b2b43d76e9f8f2c8551e3bc824e3dfad86c983
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-25 21:54:53 +0530

    Add files via upload

---
+++ String/stringprograms/ReplaceArrayWith99.java
@@ -0,0 +1,12 @@
+package com.kn.stringprograms;
+
+public class ReplaceArrayWith99 {
+
+	public static void main(String[] args) {
+		String str = "My name is Rohan";
+		
+		System.out.println(str.replace(" ", "99"));
+
+	}
+
+}
