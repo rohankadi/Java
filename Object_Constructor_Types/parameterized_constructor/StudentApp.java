b60c00b7a6a75791e32ceae5430c5ccabf18dd1c
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-29 21:41:44 +0530

    Add files via upload

---
+++ Object_Constructor_Types/parameterized_constructor/StudentApp.java
@@ -0,0 +1,14 @@
+package com.kn.parameterized_constructor;
+
+public class StudentApp {
+
+	public static void main(String[] args) {
+	//object creation
+	Student s1 = new Student("Rohan", 23);
+	
+	s1.printInfo();
+	s1.read();
+	
+	}
+
+}
