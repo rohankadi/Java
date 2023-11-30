fd91c7fd368308d12bbc94f423a818d663253a26
Rohan Kadi <118832951+rohankadi@users.noreply.github.com>
2023-11-30 20:04:00 +0530

    Add files via upload

---
+++ Practice/array/Array_EqualsOrNot2.java
@@ -0,0 +1,30 @@
+package com.kn.array;
+
+public class Array_EqualsOrNot2 {
+
+	public static void main(String[] args) {
+		int[] arr = {10, 20, 40};
+		int[] brr = {10, 20, 30};
+
+		boolean result = true;
+		//checking the length of both arrays
+		if(arr.length == brr.length) {
+			for(int i=0; i<arr.length; i++) {
+				if(arr[i] != brr[i]) {
+					result = false;
+				}
+			  }
+			}
+		else { 
+			result = false;
+		}
+		
+		if(result==true) {
+			System.out.println("Both Arrays are Equals");
+		}
+		else {
+			System.out.println("Both Arrays are not Equals");
+		}
+	}
+
+}
